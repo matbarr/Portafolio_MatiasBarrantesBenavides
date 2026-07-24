a7875e2a3746a912df07bc28ba187c9875b1ca44
matbarr <matibarra@yahoo.com>
2026-07-23 20:15:11 -0600

    semana 8
    Web y Patrones (Consultas Derivadas, JPQL y SQL)

--- Tienda_MatiasBarrantes/src/main/java/com/Tienda/repository/ProductoRepository.java
+++ Tienda_MatiasBarrantes/src/main/java/com/Tienda/repository/ProductoRepository.java
@@ -6,12 +6,26 @@ package com.Tienda.repository;
 
 import com.Tienda.domain.Producto;
 import java.util.List;
+import java.math.BigDecimal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 
-    // Método personalizado: retorna solo las categorías activas
-    public List<Producto> findByActivoTrue();
+     public List<Producto> findByActivoTrue();
+
+    //Consulta derivada que recupera los producto de un rango de precio y los ordena por precio ascendentemente
+    public List<Producto> findByPrecioBetweenOrderByPrecioAsc(BigDecimal precioInf, BigDecimal precioSup);
+    
+    //Consulta JPQL que recupera los producto de un rango de precio y los ordena por precio ascendentemente
+    @Query(value = "SELECT p FROM Producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio ASC")
+    public List<Producto> consultaJPQL(BigDecimal precioInf, BigDecimal precioSup);
+    
+    //Consulta SQL que recupera los producto de un rango de precio y los ordena por precio ascendentemente
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio ASC")
+    public List<Producto> consultaSQL(BigDecimal precioInf, BigDecimal precioSup);
+
 }
