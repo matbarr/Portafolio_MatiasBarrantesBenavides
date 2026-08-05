b97d9d7848526727ea4b922d367e37d899f628d7
matbarr <matibarra@yahoo.com>
2026-08-05 14:39:08 -0600

    semana 11
    CRUD de Usuarios + Módulo de Registro

---
+++ Tienda_MatiasBarrantes/src/main/java/com/Tienda/repository/RolRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Tienda.repository;
+
+import com.Tienda.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
+
+}
